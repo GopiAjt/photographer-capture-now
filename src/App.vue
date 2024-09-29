a9c941f926c0933e783149cc397baa687ecbaf75
GopiAjt <gopiajt23@gmail.com>
2024-09-29 20:06:07 +0530

    add: login llogout feature

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,13 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import Login from "@/views/Login.vue";
+import HomeView from './views/HomeView.vue';
 </script>
 
 <template>
-  <Login></Login>
-  <RouterLink to="/">Home</RouterLink>
-  <RouterLink to="/about">About</RouterLink>
-
-  <RouterView />
+  <Login v-if="!$store.state.isLogedIn"></Login>
+  <HomeView v-else></HomeView>
+  <!-- <RouterView /> -->
 </template>
 
 <style scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,15 @@
-<script setup>
-
-</script>
-
 <template>
-  
+    <h1>this is home</h1>
+    <Button label="logout" @click="handleLogout" />
 </template>
+
+<script>
+
+export default {
+    methods: {
+        handleLogout(){
+            this.$store.dispatch('logout');
+        }
+    }
+}
+</script>
